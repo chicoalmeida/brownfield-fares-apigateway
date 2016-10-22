10fd9014738a75f80b55bea2465113c6d8cb745b
Chico Almeida <francisco.almeida@outlook.com>
2016-10-22 12:12:25 -0200

    Adding to Service Fare a Api Gateway

---
+++ src/main/java/com/brownfield/fares/apigateway/BrownFieldFareApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.brownfield.fares.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableSwagger2
+public class BrownFieldFareApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrownFieldFareApiGatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/brownfield/fares/apigateway/BrownFieldFareApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brownfield.fares.apigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrownFieldFareApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
